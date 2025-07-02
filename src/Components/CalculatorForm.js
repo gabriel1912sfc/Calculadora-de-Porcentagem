55f2e836a3b773b070c7e57ed2536b12f5d72ffc
Gabriel Santos <gfonseca882@gmail.com>
2025-07-02 18:40:57 -0300

    Primeiro Commit: criação dos primeiros componentes

---
+++ src/Components/CalculatorForm.js
@@ -0,0 +1,15 @@
+function Calculator(){
+  return (
+    <form>
+      <label>Valor:</label>
+      <input type="number"></input>
+
+      <label>Porcentagem:</label>
+      <input type="number"></input>
+
+      <button type="submit">Calcular</button>
+    </form>
+  )
+}
+
+export default Calculator
---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+function Header({children}){
+  return (
+    <div>
+      <h1>{children}</h1>
+    </div>
+  )
+}
+export default Header;
